fc72ea9f119885d14671601a975eb7ca18e4101b
Mohab Alnajjar <72289694+mo662008@users.noreply.github.com>
2022-06-15 10:43:47 +0200

    Update 1- TwoSum.cs

--- 1- TwoSum.cs
+++ 1- TwoSum.cs
@@ -2,23 +2,19 @@ public class Solution
 {
 	public int[] TwoSum(int[] nums, int target)
 	{
-		int[] ans = new int[2];
 		var dict = new Dictionary<int, int>();
-		for (int i = 0; i < nums.Length; i++)
-		{
-			dict[nums[i]] = i;
-		}
-
 		for (int i = 0; i < nums.Length; i++)
 		{
 			int num = target - nums[i];
 			if (dict.ContainsKey(num) && dict[num] != i)
 			{
-				ans[0] = i;
-				ans[1] = dict[num];
-				break;
+				return new int[]
+				{
+					dict[num], i
+				};
 			}
+			dict[nums[i]] = i;
 		}
-		return ans;
+		return null;
 	}
 }
