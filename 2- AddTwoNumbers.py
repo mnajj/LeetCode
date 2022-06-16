a78587358633f51692a1f0c3f40e3bff428aef53
Mohab Alnajjar <72289694+mo662008@users.noreply.github.com>
2022-06-16 22:28:41 +0200

    Update 2- AddTwoNumbers.py

--- 2- AddTwoNumbers.py
+++ 2- AddTwoNumbers.py
@@ -1,3 +1,11 @@
+"""
+Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+"""
+
 # straightforward
 class Solution(object):
     def addTwoNumbers(self, l1, l2):
