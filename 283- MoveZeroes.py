62ba4280bd96b50c5f9cb1ff54158a9be73f90ef
Mohab Alnajjar <72289694+mo662008@users.noreply.github.com>
2022-06-27 01:00:45 +0200

    Update 283- MoveZeroes.py

--- 283- MoveZeroes.py
+++ 283- MoveZeroes.py
@@ -13,3 +13,19 @@ class Solution:
                 head += 1
         for i in range(0, n):
             nums[i] = temp[i]
+        return None
+            
+# Effective
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        n = len(nums)
+        i = h = 0
+        while i < n:
+            if nums[i] != 0:
+                nums[h] = nums[i]
+                h += 1
+            i += 1
+        while h < n:
+            nums[h] = 0
+            h += 1
+        return None
