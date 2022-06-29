cc1900f61381d1f1d447344473bac5cffb9962cc
Mohab Alnajjar <72289694+mo662008@users.noreply.github.com>
2022-06-30 01:11:20 +0200

    Create 557- ReverseWordsInAStringIII.py

---
+++ 557- ReverseWordsInAStringIII.py
@@ -0,0 +1,19 @@
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        stack = []
+        rev = ""
+        for c in s:
+            if c != ' ':
+                stack.append(c)
+            else:
+                rev += self.free_stack(stack)
+        while stack:
+            rev += stack.pop()
+        return rev
+
+    def free_stack(self, stack: List) -> str:
+        s = ""
+        while stack:
+            s += stack.pop()
+        s += " "
+        return s
