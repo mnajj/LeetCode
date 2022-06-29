8f5a9866e6a31fca4df7c59ceb2e405a25c4caed
Mohab Alnajjar <72289694+mo662008@users.noreply.github.com>
2022-06-29 02:46:10 +0200

    Update 287- FindTheDuplicateNumber.py

--- 287- FindTheDuplicateNumber.py
+++ 287- FindTheDuplicateNumber.py
@@ -6,3 +6,20 @@ class Solution:
                 return nums[i]
             else:
                 dict[nums[i]] = True
+
+                
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        tortoise = nums[0]
+        hare = nums[0]
+        while True:
+            tortoise = nums[tortoise]
+            hare = nums[nums[hare]]
+            if tortoise == hare:
+                break
+        p1 = nums[0]
+        p2 = tortoise
+        while p1 != p2:
+            p1 = nums[p1]
+            p2 = nums[p2]
+        return p1
