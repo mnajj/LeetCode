807ac5678755f6b4acd3f2a8b4daa7dfc67f79dc
Mohab Alnajjar <72289694+mo662008@users.noreply.github.com>
2022-06-30 01:22:02 +0200

    Update 557- ReverseWordsInAStringIII.py

--- 557- ReverseWordsInAStringIII.py
+++ 557- ReverseWordsInAStringIII.py
@@ -17,3 +17,8 @@ class Solution:
             s += stack.pop()
         s += " "
         return s
+
+    
+class Solution:
+    def reverseWords(self, s: 'str') -> 'str':
+        return " ".join(i[::-1] for i in s.split())
