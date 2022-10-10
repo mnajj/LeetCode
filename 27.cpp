1ed5309e4d9b44c349a2786c1258dff7afc5062d
Mohab Alnajjar <72289694+mo662008@users.noreply.github.com>
2022-10-10 06:13:30 +0200

    Update 27.cpp

--- 27.cpp
+++ 27.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
 		int removeElement(vector<int> &nums, int val) {
-			int k = 0, i = 0;
+			int i = 0;
 			while (i < nums.size()) {
 				if (nums[i] == val) {
 					nums.erase(nums.begin() + i);
 					continue;
 				}
-				++k;
 				++i;
 			}
-			return k;
+			return i;
 		}
 };
