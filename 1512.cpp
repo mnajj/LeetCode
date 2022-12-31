01b0ef763a1b1d3d76021723d6c37d93787d6042
Mohab Alnajjar <72289694+mnajj@users.noreply.github.com>
2022-12-31 20:52:38 +0200

    Update 1512.cpp

--- 1512.cpp
+++ 1512.cpp
@@ -4,7 +4,9 @@ public:
         int arr[101] = {0};
         int sum = 0;
         for (int x : nums) ++arr[x];
-        for (int x : arr) sum += x * (x - 1) / 2;
+        for (int i = 0; i < nums.size(); ++i) {
+            sum += arr[i] * (arr[i] - 1) / 2;
+        }
         return sum;
     }
 };
