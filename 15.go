3919744abcd2c1853962bd74800a09bba273828f
Mohab Alnajjar <72289694+mnajj@users.noreply.github.com>
2023-04-15 01:39:34 +0200

    Create 15.go

---
+++ 15.go
@@ -0,0 +1,27 @@
+func threeSum(nums []int) [][]int {
+	var result [][]int
+	sort.Slice(nums, func(i, j int) bool {
+		return nums[i] < nums[j]
+	})
+	for i := 0; i < len(nums); i++ {
+		if i > 0 && nums[i] == nums[i-1] {
+			continue
+		}
+		l, r := i+1, len(nums)-1
+		for l < r {
+			sum := nums[i] + nums[l] + nums[r]
+			if sum > 0 {
+				r--
+			} else if sum < 0 {
+				l++
+			} else {
+				result = append(result, []int{nums[i], nums[l], nums[r]})
+				l++
+				for nums[l] == nums[l-1] && l < r {
+					l++
+				}
+			}
+		}
+	}
+	return result
+}
