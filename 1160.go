344fe8e28f0472201194c26b7b78e9a33685332a
Mohab Alnajjar <72289694+mnajj@users.noreply.github.com>
2023-05-08 18:14:16 +0300

    Update 1160.go

--- 1160.go
+++ 1160.go
@@ -29,3 +29,32 @@ func mapCopy(m map[rune]int) map[rune]int {
 	}
 	return c
 }
+
+// 2
+func countCharacters(words []string, chars string) int {
+	var sum int
+	m := map[rune]int{}
+	for _, r := range chars {
+		if _, ok := m[r]; !ok {
+			m[r] = 0
+		}
+		m[r]++
+	}
+	Main:
+	for _, w := range words {
+		o := map[rune]int{}
+		for _, r := range w {
+			if _, ok := o[r]; !ok {
+				o[r] = 0
+			}
+			o[r]++
+		}
+		for k,v := range o {
+			if count, ok := m[k]; !ok || v > count {
+				continue Main
+			}
+		}
+		sum += len(w)
+	}
+	return sum
+}
