444d6ea2286d04a0018e432f7ba6c67988d57c17
Mohab Alnajjar <72289694+mnajj@users.noreply.github.com>
2024-02-24 22:05:21 +0200

    Create 836.go

---
+++ 836.go
@@ -0,0 +1,7 @@
+func isRectangleOverlap(rec1 []int, rec2 []int) bool {
+  if (rec2[2] <= rec1[0] || rec2[0] >= rec1[2]) || 
+    (rec2[3] <= rec1[1] || rec2[1] >= rec1[3]) {
+        return false
+    }
+  return true
+}
